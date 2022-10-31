8ed17db23d821d99d891cef7d3ebf4991893035f
anamontiaga <amontiaga@gmail.com>
2022-10-31 11:12:11 +0100

    heatmap exercise

--- src/exercises/geojson/points/05.stories.tsx
+++ src/exercises/geojson/points/05.stories.tsx
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+
 import { Story } from '@storybook/react/types-6-0';
+import PluginMapboxGl from '@vizzuality/layer-manager-plugin-mapboxgl';
+import { Layer, LayerManager } from '@vizzuality/layer-manager-react';
+
+import Map from 'components/map';
+import Controls from 'components/map/controls';
+import ZoomControl from 'components/map/controls/zoom';
+import { CustomMapProps } from 'components/map/types';
+import AIRPORTS_DATA from 'data/points.json';
 
 const StoryMap = {
   title: 'Exercises/Geojson/Points',
@@ -7,29 +17,107 @@ const StoryMap = {
 
 export default StoryMap;
 
-const Template: Story = () => {
+const Template: Story<CustomMapProps> = (args: CustomMapProps) => {
+  const { id, bounds, initialViewState } = args;
+
+  const [viewState, setViewState] = useState(initialViewState);
+  const AIRPORTS_LAYER = {
+    id: 'airports',
+    type: 'geojson',
+    source: {
+      type: 'geojson',
+      data: AIRPORTS_DATA,
+    },
+    images: [
+      {
+        id: 'airport',
+        src: '/images/airplane.svg',
+        options: {},
+      },
+    ],
+    render: {
+      layers: [
+        {
+          type: 'circle',
+          paint: {
+            'circle-color': '#ffCC00',
+            'circle-stroke-color': '#000000',
+            'circle-opacity': 0.5,
+            'circle-radius': 20,
+          },
+        },
+
+        {
+          type: 'heatmap',
+          paint: {
+            'heatmap-weight': 1,
+            'heatmap-intensity': ['interpolate', ['linear'], ['zoom'], 0, 1, 9, 3],
+            'heatmap-color': [
+              'interpolate',
+              ['linear'],
+              ['heatmap-density'],
+              0,
+              'rgba(255, 194, 0, 1)',
+              0.33,
+              'rgba(235, 68, 68, 1)',
+              0.66,
+              'rgba(199, 43, 214, 1)',
+              1,
+              'rgba(210, 50, 169, 1)',
+            ],
+            'heatmap-radius': 50,
+            'heatmap-opacity': 1,
+          },
+        },
+      ],
+    },
+  };
   return (
     <div className="relative w-full h-screen">
-      Draw a geojson point collection, center it on the map and display them as
-      <ul>
-        <li>Heatmap</li>
-
-        <li>Circle</li>
-        <li>color: #ffCC00</li>
-        <li>border: #000000</li>
-        <li>radius: 20</li>
-        <li>opacity: 0.5</li>
-
-        <li>Heatmap</li>
-        <li>color: #00CC00</li>
-        <li>intensity: 1</li>
-        <li>opacity: 1</li>
-        <li>radius: 50</li>
-        <li>weight: 1</li>
-      </ul>
+      <Map
+        id={id}
+        bounds={bounds}
+        viewState={viewState}
+        mapboxAccessToken={process.env.STORYBOOK_MAPBOX_API_TOKEN}
+        onMapViewStateChange={(v) => {
+          setViewState(v);
+        }}
+      >
+        {(map) => {
+          return (
+            <>
+              <LayerManager map={map} plugin={PluginMapboxGl}>
+                <Layer key={AIRPORTS_LAYER.id} {...AIRPORTS_LAYER} />
+              </LayerManager>
+              <Controls>
+                <ZoomControl id={id} />
+              </Controls>
+            </>
+          );
+        }}
+      </Map>
     </div>
   );
 };
 
-export const Points05 = Template.bind({});
-Points05.args = {};
+export const AirportHeatmap06 = Template.bind({});
+AirportHeatmap06.args = {
+  id: 'airports-map',
+  className: '',
+  viewport: {},
+  initialViewState: {},
+  bounds: {
+    bbox: [10.9588623046875, 10.5194091796875, 44.01257086123085, 43.6499881760459],
+    options: { padding: 50 },
+    viewportOptions: { transitionDuration: 0 },
+  },
+  onMapViewportChange: (viewport) => {
+    console.info('onMapViewportChange: ', viewport);
+  },
+  onMapReady: ({ map, mapContainer }) => {
+    console.info('onMapReady: ', map, mapContainer);
+  },
+  onMapLoad: ({ map, mapContainer }) => {
+    console.info('onMapLoad: ', map, mapContainer);
+  },
+};
