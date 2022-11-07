ffe8e34de10612f721efc673ae904ee89434faae
mluena <maluenarod@gmail.com>
2022-11-07 09:31:50 +0100

    maps

--- src/exercises/geojson/lines/01.stories.tsx
+++ src/exercises/geojson/lines/01.stories.tsx
@@ -1,4 +1,14 @@
+import { useState } from 'react';
+
 import { Story } from '@storybook/react/types-6-0';
+import PluginMapboxGl from '@vizzuality/layer-manager-plugin-mapboxgl';
+import { Layer, LayerManager } from '@vizzuality/layer-manager-react';
+
+import Map from 'components/map';
+import Controls from 'components/map/controls';
+import ZoomControl from 'components/map/controls/zoom';
+import { CustomMapProps } from 'components/map/types';
+import data from 'data/valencia.json';
 
 const StoryMap = {
   title: 'Exercises/Geojson/Lines',
@@ -7,19 +17,90 @@ const StoryMap = {
 
 export default StoryMap;
 
-const Template: Story = () => {
+const Template: Story<CustomMapProps> = (args: CustomMapProps) => {
+  const { id, bounds, initialViewState } = args;
+  const [viewState, setViewState] = useState(initialViewState);
+
+  const LAYER = {
+    id: 'valencia-provinces',
+    type: 'vector',
+    source: {
+      data,
+      type: 'geojson',
+    },
+    render: {
+      layers: [
+        {
+          id: 'route',
+          type: 'line',
+          layout: {
+            'line-join': 'round',
+            'line-cap': 'round',
+          },
+          paint: {
+            'line-color': '#ffCC00',
+            'line-width': 5,
+            'line-opacity': 0.5,
+          },
+        },
+      ],
+    },
+  };
   return (
     <div className="relative w-full h-screen">
-      Draw a geojson linestring collection, center it on the map and display them as
-      <ul>
-        <li>Lines</li>
-        <li>color: #ffCC00</li>
-        <li>width: 5</li>
-        <li>opacity: 0.5</li>
-      </ul>
+      <div className="prose dark:prose-invert">
+        Draw a geojson linestring collection, center it on the map and display them as
+        <ul>
+          <li>Lines</li>
+          <li>color: #ffCC00</li>
+          <li>width: 5</li>
+          <li>opacity: 0.5</li>
+        </ul>
+      </div>
+      <Map
+        id={id}
+        bounds={bounds}
+        viewState={viewState}
+        mapboxAccessToken={process.env.STORYBOOK_MAPBOX_API_TOKEN}
+        onMapViewStateChange={(v) => {
+          setViewState(v);
+        }}
+      >
+        {(map) => {
+          return (
+            <>
+              <LayerManager map={map} plugin={PluginMapboxGl}>
+                <Layer key={LAYER.id} {...LAYER} />
+              </LayerManager>
+              <Controls>
+                <ZoomControl id={id} />
+              </Controls>
+            </>
+          );
+        }}
+      </Map>
     </div>
   );
 };
 
 export const Lines01 = Template.bind({});
-Lines01.args = {};
+Lines01.args = {
+  id: 'valencia-provinces',
+  className: '',
+  viewport: {},
+  initialViewState: {},
+  bounds: {
+    bbox: [-0.477576, 39.389689, -0.257849, 39.542355],
+    options: { padding: 50 },
+    viewportOptions: { transitionDuration: 0 },
+  },
+  onMapViewportChange: (viewport) => {
+    console.info('onMapViewportChange: ', viewport);
+  },
+  onMapReady: ({ map, mapContainer }) => {
+    console.info('onMapReady: ', map, mapContainer);
+  },
+  onMapLoad: ({ map, mapContainer }) => {
+    console.info('onMapLoad: ', map, mapContainer);
+  },
+};
