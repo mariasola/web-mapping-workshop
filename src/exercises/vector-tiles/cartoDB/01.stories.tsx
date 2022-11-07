a0523aa6222be5bd456e5d46cfa4771838b9a032
anamontiaga <amontiaga@gmail.com>
2022-11-07 14:53:36 +0100

    add maps

--- src/exercises/vector-tiles/cartoDB/01.stories.tsx
+++ src/exercises/vector-tiles/cartoDB/01.stories.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+
 import { Story } from '@storybook/react/types-6-0';
+import PluginMapboxGl from '@vizzuality/layer-manager-plugin-mapboxgl';
+import { Layer, LayerManager } from '@vizzuality/layer-manager-react';
 
 import Map from 'components/map';
+import Controls from 'components/map/controls';
+import ZoomControl from 'components/map/controls/zoom';
+import { CustomMapProps } from 'components/map/types';
+import AIRPORTS_DATA from 'data/points.json';
 
 const StoryMap = {
   title: 'Exercises/Vector Tiles/Carto DB',
@@ -10,10 +18,59 @@ const StoryMap = {
 
 export default StoryMap;
 
-const Template: Story = () => {
+const Template: Story<CustomMapProps> = (args: CustomMapProps) => {
+  const { id, bounds, initialViewState } = args;
+
+  const [viewState, setViewState] = useState(initialViewState);
+
+  const CARTODB_LAYER = {
+    id: 'airports',
+    type: 'geojson',
+    source: {
+      type: 'geojson',
+      data: AIRPORTS_DATA,
+      cluster: true,
+      clusterMaxZoom: 14, // Max zoom to cluster points on
+      clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+    },
+    render: {
+      layers: [
+        {
+          type: 'circle',
+          id: 'clusters',
+          filter: ['has', 'point_count'],
+          paint: {
+            'circle-stroke-color': '#000000',
+            'circle-opacity': 0.5,
+            'circle-radius': 20,
+            'circle-color': [
+              'step',
+              ['get', 'point_count'],
+              '#51bbd6',
+              25,
+              '#f1f075',
+              50,
+              '#f28cb1',
+            ],
+          },
+        },
+        {
+          id: 'cluster-count',
+          type: 'symbol',
+          filter: ['has', 'point_count'],
+          layout: {
+            'text-field': '{point_count_abbreviated}',
+            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+            'text-size': 12,
+          },
+        },
+      ],
+    },
+  };
+
   return (
     <div className="relative w-full h-screen">
-      Draw a tiles layer, center it on the map and display them as
+      Draw a tiles layer with a CartoDB tileset, center it on the map and display them as
       <ul>
         {/* <li>Circles</li>
         <li>color: #ffCC00</li>
@@ -21,9 +78,50 @@ const Template: Story = () => {
         <li>radius: 20</li>
         <li>opacity: 0.5</li> */}
       </ul>
+      <Map
+        id={id}
+        bounds={bounds}
+        viewState={viewState}
+        mapboxAccessToken={process.env.STORYBOOK_MAPBOX_API_TOKEN}
+        onMapViewStateChange={(v) => {
+          setViewState(v);
+        }}
+      >
+        {(map) => {
+          return (
+            <>
+              <LayerManager map={map} plugin={PluginMapboxGl}>
+                <Layer key={CARTODB_LAYER.id} {...CARTODB_LAYER} />
+              </LayerManager>
+              <Controls>
+                <ZoomControl id={id} />
+              </Controls>
+            </>
+          );
+        }}
+      </Map>
     </div>
   );
 };
 
 export const CartoDB01 = Template.bind({});
-CartoDB01.args = {};
+CartoDB01.args = {
+  id: 'tiles-map',
+  className: '',
+  viewport: {},
+  initialViewState: {},
+  bounds: {
+    bbox: [-154.335938, -63.548552, 154.335938, 63.548552],
+    options: { padding: 50 },
+    viewportOptions: { transitionDuration: 0 },
+  },
+  onMapViewportChange: (viewport) => {
+    console.info('onMapViewportChange: ', viewport);
+  },
+  onMapReady: ({ map, mapContainer }) => {
+    console.info('onMapReady: ', map, mapContainer);
+  },
+  onMapLoad: ({ map, mapContainer }) => {
+    console.info('onMapLoad: ', map, mapContainer);
+  },
+};
--- src/exercises/vector-tiles/external/01.stories.tsx
+++ src/exercises/vector-tiles/external/01.stories.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+
 import { Story } from '@storybook/react/types-6-0';
+import PluginMapboxGl from '@vizzuality/layer-manager-plugin-mapboxgl';
+import { Layer, LayerManager } from '@vizzuality/layer-manager-react';
 
 import Map from 'components/map';
+import Controls from 'components/map/controls';
+import ZoomControl from 'components/map/controls/zoom';
+import { CustomMapProps } from 'components/map/types';
+import AIRPORTS_DATA from 'data/points.json';
 
 const StoryMap = {
   title: 'Exercises/Vector Tiles/External',
@@ -10,7 +18,56 @@ const StoryMap = {
 
 export default StoryMap;
 
-const Template: Story = () => {
+const Template: Story<CustomMapProps> = (args: CustomMapProps) => {
+  const { id, bounds, initialViewState } = args;
+
+  const [viewState, setViewState] = useState(initialViewState);
+
+  const EXTERNAL_LAYER = {
+    id: 'airports',
+    type: 'geojson',
+    source: {
+      type: 'geojson',
+      data: AIRPORTS_DATA,
+      cluster: true,
+      clusterMaxZoom: 14, // Max zoom to cluster points on
+      clusterRadius: 50, // Radius of each cluster when clustering points (defaults to 50)
+    },
+    render: {
+      layers: [
+        {
+          type: 'circle',
+          id: 'clusters',
+          filter: ['has', 'point_count'],
+          paint: {
+            'circle-stroke-color': '#000000',
+            'circle-opacity': 0.5,
+            'circle-radius': 20,
+            'circle-color': [
+              'step',
+              ['get', 'point_count'],
+              '#51bbd6',
+              25,
+              '#f1f075',
+              50,
+              '#f28cb1',
+            ],
+          },
+        },
+        {
+          id: 'cluster-count',
+          type: 'symbol',
+          filter: ['has', 'point_count'],
+          layout: {
+            'text-field': '{point_count_abbreviated}',
+            'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+            'text-size': 12,
+          },
+        },
+      ],
+    },
+  };
+
   return (
     <div className="relative w-full h-screen">
       Draw a tiles layer, center it on the map and display them as
@@ -21,9 +78,50 @@ const Template: Story = () => {
         <li>radius: 20</li>
         <li>opacity: 0.5</li> */}
       </ul>
+      <Map
+        id={id}
+        bounds={bounds}
+        viewState={viewState}
+        mapboxAccessToken={process.env.STORYBOOK_MAPBOX_API_TOKEN}
+        onMapViewStateChange={(v) => {
+          setViewState(v);
+        }}
+      >
+        {(map) => {
+          return (
+            <>
+              <LayerManager map={map} plugin={PluginMapboxGl}>
+                <Layer key={EXTERNAL_LAYER.id} {...EXTERNAL_LAYER} />
+              </LayerManager>
+              <Controls>
+                <ZoomControl id={id} />
+              </Controls>
+            </>
+          );
+        }}
+      </Map>
     </div>
   );
 };
 
 export const External01 = Template.bind({});
-External01.args = {};
+External01.args = {
+  id: 'tiles-map',
+  className: '',
+  viewport: {},
+  initialViewState: {},
+  bounds: {
+    bbox: [-154.335938, -63.548552, 154.335938, 63.548552],
+    options: { padding: 50 },
+    viewportOptions: { transitionDuration: 0 },
+  },
+  onMapViewportChange: (viewport) => {
+    console.info('onMapViewportChange: ', viewport);
+  },
+  onMapReady: ({ map, mapContainer }) => {
+    console.info('onMapReady: ', map, mapContainer);
+  },
+  onMapLoad: ({ map, mapContainer }) => {
+    console.info('onMapLoad: ', map, mapContainer);
+  },
+};
