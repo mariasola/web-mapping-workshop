1d9a1c54f76bdc2df8aa3e5242e6264325b9df55
anamontiaga <amontiaga@gmail.com>
2022-11-11 14:41:23 +0100

    add interpolate zoom color exercise to points

---
+++ src/exercises/geojson/points/07.stories.tsx
@@ -0,0 +1,133 @@
+import { useState } from 'react';
+
+import { ViewState } from 'react-map-gl';
+
+import { Story } from '@storybook/react/types-6-0';
+import PluginMapboxGl from '@vizzuality/layer-manager-plugin-mapboxgl';
+import { Layer, LayerManager } from '@vizzuality/layer-manager-react';
+
+import Code from 'components/code';
+import Map from 'components/map';
+import Controls from 'components/map/controls';
+import ZoomControl from 'components/map/controls/zoom';
+import { CustomMapProps } from 'components/map/types';
+import AIRPORTS_DATA from 'data/airports.json';
+const StoryMap = {
+  title: 'Exercises/Geojson/Points',
+  argTypes: {},
+};
+
+export default StoryMap;
+
+const Template: Story<CustomMapProps> = (args: CustomMapProps) => {
+  const { id, bounds, initialViewState } = args;
+
+  const [viewState, setViewState] = useState<Partial<ViewState>>();
+
+  const AIRPORTS_LAYER = {
+    id: 'airports',
+    type: 'geojson',
+    source: {
+      type: 'geojson',
+      data: AIRPORTS_DATA,
+    },
+    images: [
+      {
+        id: 'airport',
+        src: '/images/airplane.svg',
+        options: {},
+      },
+    ],
+    render: {
+      layers: [
+        {
+          type: 'circle',
+          paint: {
+            'circle-color': [
+              'interpolate',
+              ['linear'],
+              ['zoom'],
+              1,
+              '#FF0000',
+              2,
+              '#FFFF00',
+              3,
+              '#00FF00',
+              4,
+              '#FFC0CB',
+              5,
+              '#0000FF',
+            ],
+            'circle-radius': 3,
+          },
+        },
+      ],
+    },
+  };
+
+  return (
+    <>
+      <div className="prose">
+        <h2>Geojson: Points 07</h2>
+        <p>
+          With this{' '}
+          <a
+            href="https://github.com/codeforgermany/click_that_hood/blob/main/public/data/airports.geojson"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Geojson
+          </a>
+          , draw a point collection, center it on the map and display them so that they gradually
+          change color as you zoom in, using the following color ramp:
+        </p>
+        <Code>{`ramp = ['#FF0000', '#FFFF00', '#00FF00', '#FFC0CB', '#0000FF']`}</Code>
+      </div>
+      <Map
+        id={id}
+        bounds={bounds}
+        initialViewState={initialViewState}
+        viewState={viewState}
+        mapboxAccessToken={process.env.STORYBOOK_MAPBOX_API_TOKEN}
+        onMapViewStateChange={(v) => {
+          setViewState(v);
+        }}
+      >
+        {(map) => {
+          return (
+            <>
+              <LayerManager map={map} plugin={PluginMapboxGl}>
+                <Layer key={AIRPORTS_LAYER.id} {...AIRPORTS_LAYER} />
+              </LayerManager>
+              <Controls>
+                <ZoomControl id={id} />
+              </Controls>
+            </>
+          );
+        }}
+      </Map>
+    </>
+  );
+};
+
+export const Points07 = Template.bind({});
+Points07.args = {
+  id: 'airports-map',
+  className: '',
+  viewport: {},
+  initialViewState: {
+    bounds: [-154.335938, -63.548552, 154.335938, 63.548552],
+    fitBoundsOptions: {
+      padding: 50,
+    },
+  },
+  onMapViewportChange: (viewport) => {
+    console.info('onMapViewportChange: ', viewport);
+  },
+  onMapReady: ({ map, mapContainer }) => {
+    console.info('onMapReady: ', map, mapContainer);
+  },
+  onMapLoad: ({ map, mapContainer }) => {
+    console.info('onMapLoad: ', map, mapContainer);
+  },
+};
